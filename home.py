186e67b29910f3f90716867d3247d957a2413327
Big165 <Seagame.w2005@gmail.com>
2025-03-18 11:18:26 +0700

    .

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.datasets import load_iris
 
 # โหลดชุดข้อมูล Heart
-Heart = pd.read_csv('./data/heart3.csv')
+Heart = pd.read_csv('./data/heart.csv')
 X = Heart.drop(columns=['HeartDisease'])
 y = Heart.HeartDisease
 
@@ -18,23 +18,34 @@ st.title("Naïve Bayes Classifier - Iris Dataset")
 st.write("ป้อนคุณสมบัติของดอกไม้เพื่อทำนายประเภท")
 
 # รับค่าจากผู้ใช้ผ่าน slider
-sepal_length = st.slider("Sepal Length (cm)", float(X[:,0].min()), float(X[:,0].max()), float(X[:,0].mean()))
-sepal_width = st.slider("Sepal Width (cm)", float(X[:,1].min()), float(X[:,1].max()), float(X[:,1].mean()))
-petal_length = st.slider("Petal Length (cm)", float(X[:,2].min()), float(X[:,2].max()), float(X[:,2].mean()))
-petal_width = st.slider("Petal Width (cm)", float(X[:,3].min()), float(X[:,3].max()), float(X[:,3].mean()))
+ A1 = st.number_input("ข้อมูล1")
+ A2 = st.number_input("ข้อมูล2")
+ A3 = st.number_input("ข้อมูล3")
+ A4 = st.number_input("ข้อมูล4")
+ A5 = st.number_input("ข้อมูล5")
+ A6 = st.number_input("ข้อมูล6")
+ A7 = st.number_input("ข้อมูล7")
+ A8 = st.number_input("ข้อมูล8")
+ A9 = st.number_input("ข้อมูล9")
+ A10 = st.number_input("ข้อมูล10")
+ A11 = st.number_input("ข้อมูล11")
+
 
 # สร้างอาร์เรย์ข้อมูลจากค่าที่ป้อน
-input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+input_data = np.array([[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11]])
 
 # ทำนายผลลัพธ์
 prediction = model.predict(input_data)
 prediction_proba = model.predict_proba(input_data)
 
+
+
+
 # แสดงผลลัพธ์
 st.subheader("ผลลัพธ์ที่ได้:")
-st.write(f"ชนิดของดอกไม้ที่คาดการณ์: *{iris.target_names[prediction[0]]}*")
+st.write(f"คุณเป็นโรคหัวใจรึเปล่า?: *{prediction[0]}*")
 
 # แสดงความน่าจะเป็นของแต่ละคลาส
 st.subheader("ความน่าจะเป็นของแต่ละประเภท:")
-df_proba = pd.DataFrame(prediction_proba, columns=iris.target_names)
+df_proba = pd.DataFrame(prediction_proba, columns=iris.HeartDisease)
 st.dataframe(df_proba.style.format("{:.2%}"))
